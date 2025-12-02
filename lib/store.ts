a417f1108e04538597d46b7e0b230f58f81b2e70
ray03j <eihouse.potato@outlook.jp>
2025-12-02 19:22:30 +0900

    delete: 不要となったSource, Material, Editorのsliceファイルとstore.tsのimportを削除

--- lib/store.ts
+++ lib/store.ts
@@ -2,11 +2,8 @@ import { create } from 'zustand';
 import { persist } from 'zustand/middleware';
 import { AppStore } from './store/slices/types';
 import { createUiSlice } from './store/slices/uiSlice';
-import { createSourceSlice } from './store/slices/sourceSlice';
-import { createMaterialSlice } from './store/slices/materialSlice';
 import { createChatSlice } from './store/slices/chatSlice';
 import { createBookSlice } from './store/slices/bookSlice';
-import { createEditorSlice } from './store/slices/editorSlice';
 
 const partialize = (state: AppStore) => ({
   books: state.books,
@@ -24,11 +21,8 @@ export const useStore = create<AppStore>()(
   persist(
     (...a) => ({
       ...createUiSlice(...a),
-      ...createSourceSlice(...a),
-      ...createMaterialSlice(...a),
       ...createChatSlice(...a),
-      ...createBookSlice(...a),
-      ...createEditorSlice(...a)
+      ...createBookSlice(...a)
     }),
     {
       name: 'app-storage',
